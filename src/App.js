84b7c583e80b3303149eb5b24ec3579be248a0a7
TAEJU <TAEJU@naver.com>
2022-03-27 17:45:40 +0900

    다이어리 리스트 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
+const dummyList = [
+  {
+    id: 1,
+    author: "김태주",
+    content: "하이 1",
+    emotion: 1,
+    // 현재 시간을 기준으로 생성
+    created_date: new Date().getTime(),
+  },
+
+  {
+    id: 2,
+    author: "김길동",
+    content: "하이 2",
+    emotion: 3,
+    // 현재 시간을 기준으로 생성
+    created_date: new Date().getTime(),
+  },
+
+  {
+    id: 3,
+    author: "홍길동",
+    content: "하이 3",
+    emotion: 1,
+    // 현재 시간을 기준으로 생성
+    created_date: new Date().getTime(),
+  },
+];
+
+// dummydata를 DiaryList에diaryList라는 이름의 prop으로 전달
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,8 +1,6 @@
 import React, { useRef, useState } from "react";
 
 const DiaryEditor = () => {
-  // useRef 함수를 호출해 반환값을 authorInput에 담아줌
-  // authorInput은 MutableRefObject인데 html dom 요소에 접근할 수 있는 기능을 함
   const authorInput = useRef();
   const contentInput = useRef();
 
@@ -21,13 +19,11 @@ const DiaryEditor = () => {
 
   const handleSubmit = () => {
     if (state.author.length < 1) {
-      // focus
       authorInput.current.focus();
       return;
     }
 
     if (state.content.length < 5) {
-      // focus
       contentInput.current.focus();
       return;
     }
@@ -39,9 +35,6 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
-          // authorInput이라는 객체를 통해서input 태그에 접근 가능
-          // userRef로 생성한 authorInput같은 레퍼런스 객체는 현재 가르키는 값을 current라는 프로퍼티로 불러와 사용할 수 있음
-          // authorInput.current는 authorInput 태그가 되는거고 focus라는 기능을 사용해 focus 되도록 만든것
           ref={authorInput}
           name="author"
           value={state.author}
