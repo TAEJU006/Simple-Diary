08445fb4e9865953444a72f57dc228ce9e8f21c8
TAEJU <TAEJU@naver.com>
2022-03-28 19:29:16 +0900

    optimize 삭제전 commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import OptimizeTest from "./OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
@@ -68,7 +67,6 @@ function App() {
 
   return (
     <div className="App">
-      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체일기 : {data.length}</div>
       <div>기분 좋은 일기 갯수 : {goodCount}</div>
